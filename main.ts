1ef2b9154d3fdcfda515cbe3b91ef95535195a24
Microbit321 <96684814+TimKapunkt@users.noreply.github.com>
2024-02-17 16:33:59 +0100

    Aufhängen behoben.

--- main.ts
+++ main.ts
@@ -1,8 +1,8 @@
-input.onLogoEvent(TouchButtonEvent.Pressed, function () {
-    sprite.change(LedSpriteProperty.Y, -1)
-})
 input.onButtonPressed(Button.A, function () {
     sprite.change(LedSpriteProperty.X, -1)
+})
+input.onGesture(Gesture.Shake, function () {
+    sprite.change(LedSpriteProperty.Y, -1)
 })
 input.onButtonPressed(Button.AB, function () {
     sprite.change(LedSpriteProperty.Y, 1)
@@ -10,8 +10,6 @@ input.onButtonPressed(Button.AB, function () {
 input.onButtonPressed(Button.B, function () {
     sprite.change(LedSpriteProperty.X, 1)
 })
-let punkty = 0
-let punktx = 0
 let sprite: game.LedSprite = null
 let x = randint(0, 4)
 let y = randint(0, 4)
@@ -25,34 +23,10 @@ basic.showString("A+B=runter")
 basic.showString("Logo=hoch")
 sprite = game.createSprite(2, 2)
 basic.forever(function () {
-    if (sprite.get(LedSpriteProperty.X) == punktx && sprite.get(LedSpriteProperty.Y) == punkty) {
+    if (sprite.get(LedSpriteProperty.X) == x && sprite.get(LedSpriteProperty.Y) == y) {
         music.play(music.stringPlayable("C D E F G A B C5 ", 120), music.PlaybackMode.InBackground)
         music.play(music.stringPlayable("C5 B A G F E D C ", 120), music.PlaybackMode.InBackground)
         basic.showString("GEWONNEN!!!")
         control.reset()
-    }
-    if (x == 0) {
-        punktx = 0
-    }
-    if (y == 0) {
-        punkty = 0
-    }
-    if (x == 1) {
-        punktx = 1
-    }
-    if (y == 1) {
-        punkty = 1
-    }
-    if (x == 3) {
-        punktx = 3
-    }
-    if (y == 3) {
-        punkty = 3
-    }
-    if (x == 4) {
-        punktx = 4
-    }
-    if (y == 4) {
-        punkty = 4
     }
 })
